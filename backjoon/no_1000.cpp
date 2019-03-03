212e8abf0f21d0440c5acb5f2031fa33d392b49b
warguss <kuc0104@naver.com>
2019-03-03 18:59:17 +0900

    1000, 10001번

---
+++ backjoon/no_1000.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int a, b = 0;
+	scanf("%d %d", &a, &b);
+	if ( (a < 0 &&  a > 10) || (b < 0 && b > 10) )
+	{
+		return -1;
+	} 
+
+	printf("%d", a+b);
+	return 0;
+}
